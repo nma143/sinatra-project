b11c68f05819a02bed64fa9fe128290bab2ae20e
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 16:56:50 -0500

    create signup get route and view

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,14 @@
 class UsersController < ApplicationController
 
+  get '/signup' do
+    if !logged_in?
+      erb :'users/create_user'
+    else
+      "Sign up page says you're logged in"
+    end
+  end
+
+
   get '/login' do
     if !logged_in?
       erb :'users/login'
