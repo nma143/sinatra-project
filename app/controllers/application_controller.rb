b59480535cbcec7f29f9e927243fcce11965a46c
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 16:38:28 -0500

    add logged in condition to / route

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -8,20 +8,22 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/' do
-    erb :index
+    if logged_in?
+      "Logged in"
+    else
+      erb :index
+    end
   end
 
-end
+  helpers do
 
+    def logged_in?
+      !! current_user
+    end
 
-helpers do
+    def current_user
+      @current_user ||= User.find_by(id:session[:user_id]) if session[:user_id]
+    end
 
-  def logged_in?
-    !! current_user
   end
-
-  def current_user
-    @current_user ||= User.find_by(id:session[:user_id]) if session[:user_id]
-  end
-
 end
