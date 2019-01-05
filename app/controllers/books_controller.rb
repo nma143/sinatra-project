8b16e4851867fb91ddbe1392518f979d98c91178
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 18:22:15 -0500

    create /books route and book_list view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,3 +1,12 @@
 class BooksController < ApplicationController
-  
+
+  get '/books' do
+   if logged_in?
+     @books = Book.all
+     erb :'books/book_list'
+   else
+     redirect to '/'
+   end
+ end
+
 end
