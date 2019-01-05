60e3f91c5576a9c2ebacb7dfaa48ce0e61a773a0
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 18:56:46 -0500

    create reviews/new route and view

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,3 +1,11 @@
 class ReviewsController < ApplicationController
 
+  get '/reviews/new' do
+    if logged_in?
+      @books= Book.all
+      erb :'reviews/new'
+    else
+      redirect to '/'
+    end
+  end
 end
