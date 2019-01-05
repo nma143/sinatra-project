a0c379e42257859a8985aacf535c2b3aa81d45a1
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 18:28:23 -0500

    create seeds.rb file to seed db with a library of books

---
+++ db/seeds.rb
@@ -0,0 +1,5 @@
+# Seed database with a library of books
+
+Book.create(title: "Harry Potter 1", author: "JKR")
+Book.create(title: "Harry Potter 2", author: "JKR")
+Book.create(title: "Lord of the Rings", author: "JRRT")
