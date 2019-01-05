46e750e6ee42c613810c85f9d1349ff736dc0edb
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 15:22:32 -0500

    enable sessions

--- app/application_controller.rb
+++ app/application_controller.rb
@@ -3,6 +3,8 @@ class ApplicationController < Sinatra::Base
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
+    enable :sessions
+    set :session_secret, "secret_book_club"
   end
 
   get '/' do
