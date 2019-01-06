5cfcc2fcd9ed55e76769670852442b5c4278873c
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 19:47:43 -0500

    add average star value to book/slug view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -12,6 +12,8 @@ class BooksController < ApplicationController
   get '/books/:slug' do
     if logged_in?
       @book = Book.find_by_slug(params[:slug])
+      star_ratings = @book.reviews.collect {|review| review.stars}
+      @avg_star_rating = star_ratings.sum/star_ratings.size.to_f
       
       erb :'books/book_reviews'
     else
