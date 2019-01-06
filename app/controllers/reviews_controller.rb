c77f176a6d510ef4143be1d464100aa5625f1646
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 19:03:40 -0500

    create post method for new review

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -8,4 +8,26 @@ class ReviewsController < ApplicationController
       redirect to '/'
     end
   end
+
+  post '/reviews/new' do
+    if !logged_in?
+      redirect to '/'
+    else
+      if params[:content] == ""
+        #eventually show an error: "You must write what you thought of the book"
+        redirect to '/reviews/new'
+      else
+        @review = Review.new(:content => params[:content], :stars => params[:stars], :user_id => session[:user_id], :book_id => params[:book_id])
+        @review.save
+        redirect to "/books"
+      end
+    end
+  end
+
+
+
+
+
+
+
 end
