6e2d375e3db3546029484a2b21616be3b966eadc
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 19:42:56 -0500

    create slug methods for books, books/slug route, and view

--- app/controllers/books_controller.rb
+++ app/controllers/books_controller.rb
@@ -1,12 +1,22 @@
 class BooksController < ApplicationController
 
   get '/books' do
-   if logged_in?
-     @books = Book.all
-     erb :'books/book_list'
-   else
-     redirect to '/'
-   end
- end
+    if logged_in?
+      @books = Book.all
+      erb :'books/book_list'
+    else
+      redirect to '/'
+    end
+  end
+
+  get '/books/:slug' do
+    if logged_in?
+      @book = Book.find_by_slug(params[:slug])
+      
+      erb :'books/book_reviews'
+    else
+      redirect to '/'
+    end
+  end
 
 end
--- app/models/book.rb
+++ app/models/book.rb
@@ -1,5 +1,13 @@
 class Book < ActiveRecord::Base
   has_many  :reviews
   has_many :users, through: :reviews
-  
+
+  def slug
+    title.downcase.gsub(" ","-")
+  end
+
+  def self.find_by_slug(slug)
+    Book.all.find{|book| book.slug == slug}
+  end
+
 end
