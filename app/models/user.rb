4191b0aa0c457e55b96d53fddbc528b8ca309a93
nma143 <nikki.apostolakis@gmail.com>
2019-01-05 19:50:17 -0500

    add slug methods for User

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,9 +4,17 @@ class User < ActiveRecord::Base
 
   has_secure_password
 
+  def slug
+    username.downcase.gsub(" ","-")
+
+  end
+
+  def self.find_by_slug(slug)
+    User.all.find{|user| user.slug == slug}
+  end
+
   def self.find_by_email(email)
     User.all.find{|user| user.email == email}
   end
-
 
 end
